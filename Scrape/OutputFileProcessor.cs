880e8fb9546c851c43bc52ddd54f2df56a9259f6
Andrew Johnson <helios2k6@gmail.com>
2015-12-28 11:02:09 -0500

    Adding alternative parsing logic for deducing the TimeSpan

--- Scrape/OutputFileProcessor.cs
+++ Scrape/OutputFileProcessor.cs
@@ -21,6 +21,7 @@
 
 using Functional.Maybe;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 /// <summary>
@@ -88,6 +89,49 @@ internal static class OutputFileProcessor
 
     private static Maybe<TimeSpan> TryDeduceTimeCodeFromKnownDigitIssue(string primarySubstring)
     {
+        var reversedPrimarySubstring = primarySubstring.Reverse().ToArray();
+        int currentIndex = 0;
+        var secondsChars = new List<char>();
+        var minutesChars = new List<char>();
+        var hoursChars = new List<char>();
+        foreach (var currentChar in reversedPrimarySubstring)
+        {
+            if (currentIndex < 2)
+            {
+                secondsChars.Add(currentChar);
+            }
+            else if (currentIndex < 4)
+            {
+                minutesChars.Add(currentChar);
+            }
+            else if (currentIndex > 4)
+            {
+                hoursChars.Add(currentChar);
+            }
+            currentIndex++;
+        }
+        
+        secondsChars.Reverse();
+        minutesChars.Reverse();
+        hoursChars.Reverse();
+        
+        var secondsString = new string(secondsChars.ToArray());
+        var minutesString = new string(minutesChars.ToArray());
+        var hoursString = new string(hoursChars.ToArray());
+        
+        int seconds, minutes, hours;
+        seconds = minutes = hours = 0;
+        
+        if (int.TryParse(secondsString, out seconds) && int.TryParse(minutesString, out minutes))
+        {
+            if (int.TryParse(hoursString, out hours) == false)
+            {
+                hours = 0;
+            }
+            
+            return new TimeSpan(hours, minutes, seconds).ToMaybe();
+        }
+        
         return Maybe<TimeSpan>.Nothing;
     }
 }

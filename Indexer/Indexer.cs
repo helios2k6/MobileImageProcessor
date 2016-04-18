aa3a02bbe62d916a7f5ff55082795d427d0324f5
Andrew Johnson <helios2k6@gmail.com>
2016-04-17 23:01:42 -0400

    Cleaning up some code that makes no sense

--- Indexer/Indexer.cs
+++ Indexer/Indexer.cs
@@ -30,32 +30,11 @@ namespace Indexer
 {
     internal static class Indexer
     {
-        #region private classes
-        private sealed class ImageProductionJob
-        {
-            public TimeSpan Start { get; set; }
-
-            public TimeSpan Duration { get; set; }
-
-            public string VideoFile { get; set; }
-        }
-
-        private sealed class ImageProductionJobResult
-        {
-            public string VideoFile { get; set; }
-
-            public string Image { get; set; }
-
-            public TimeSpan TimeStamp { get; set; }
-        }
-        #endregion
-
         #region public methods
         public async static Task<IEnumerable<IndexEntry>> IndexVideoAsync(string videoFile)
         {
             MediaInfo info = await GetMediaInfoAsync(videoFile);
-
-            throw new NotImplementedException();
+            return await GetIndexEntriesAsync(info);
         }
         #endregion
 
@@ -68,30 +47,9 @@ namespace Indexer
             });
         }
 
-        private static IEnumerable<ImageProductionJob> CreateImageProductionJobs(string videoFile, MediaInfo mediaInfo)
+        private static Task<IEnumerable<IndexEntry>> GetIndexEntriesAsync(MediaInfo info)
         {
-            for (var i = TimeSpan.FromSeconds(0); i < mediaInfo.GetDuration(); i += TimeSpan.FromSeconds(30))
-            {
-
-            }
-            return null;
-        }
-
-        private static Task<IEnumerable<IndexEntry>> ExecuteImageProductionJobAsync(ImageProductionJob job)
-        {
-            return null;
-        }
-
-        private static Maybe<IndexEntry> TryIndexImage(ImageProductionJobResult indexingResult)
-        {
-            return from image in CommonFunctions.TryLoadImageAsLockBit(indexingResult.Image)
-                   let fingerPrint = ImageFingerPrinter.CalculateFingerPrint(image)
-                   select new IndexEntry
-                   {
-                       FrameTimeStamp = indexingResult.TimeStamp,
-                       FrameHash = fingerPrint,
-                       VideoFile = indexingResult.VideoFile,
-                   };
+            throw new NotImplementedException();
         }
         #endregion
     }
